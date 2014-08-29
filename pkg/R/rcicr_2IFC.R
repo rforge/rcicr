fb1977989fbd2a7f0abd968d146d8eeb9101c090
rdotsch <rdotsch@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-08-29 16:15:10 +0000

    Added batchGenerateCI2IFC
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcicr@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/rcicr_2IFC.R
+++ pkg/R/rcicr_2IFC.R
@@ -214,6 +214,55 @@ generateCI2IFC <- function(stimuli, responses, baseimage, rdata, saveasjpeg=TRUE
   return(list(ci=ci, scaled=scaled, base=base, combined=combined))
 }
 
+
+
+#' Generates multiple 2IFC classification images by participant or condition 
+#' 
+#' Generate classification image for 2 images forced choice reverse correlation task. 
+#' 
+#' This funcions saves the classification images by participant or condition as jpeg to a folder and returns the CIs.
+#' 
+#' @export
+#' @param data Data frame 
+#' @param by String specifying column name that specifies the smallest unit (participant, condition) to subset the data on and calculate CIs for
+#' @param stimuli String specifying column name in data frame that contains the stimulus numbers of the presented stimuli
+#' @param responses String specifying column name in data frame that contains the responses coded 1 for original stimulus selected and -1 for inverted stimulus selected.
+#' @param baseimage String specifying which base image was used. Not the file name, but the key used in the list of base images at time of generating the stimuli.
+#' @param rdata String pointing to .RData file that was created when stimuli were generated. This file contains the contrast parameters of all generated stimuli.
+#' @param saveasjpeg Boolean stating whether to additionally save the CI as jpeg image
+#' @param filename Optional string to specify a file name for the jpeg image
+#' @param antiCI Optional boolean specifying whether antiCI instead of CI should be computed
+#' @param scaling Optional string specifying scaling method: \code{none}, \code{constant},  \code{matched} or \code{autoscale} (default)
+#' @param constant Optional number specifying the value used as constant scaling factor for the noise (only works for \code{scaling='constant'})
+#' @return List of classification image data structures (which are themselves lists of pixel matrix of classification noise only, scaled classification noise only, base image only and combined) 
+batchGenerateCI2IFC <- function(data, by, stimuli, responses, baseimage, rdata, saveasjpeg=TRUE, filename='', antiCI=FALSE, scaling='autoscale', constant=0.1) {
+ 
+  if (scaling == 'autoscale') {
+    doAutoscale <- TRUE
+    scaling <- 'none'
+  } else {
+    doAutoscale <- FALSE
+  }
+  
+  cis <- list()
+  
+  for (unit in unique(data[,by])) {
+    # Get subset of data 
+    unitdata <- data[data[,by] == unit, ]
+    
+    # Compute CI with appropriate settings for this subset
+    cis[[unit]] <- generateCI2IFC(unitdata[,stimuli], unitdata[,responses], baseimage, rdata, saveasjpeg, filename, antiCI, scaling, constant)
+  
+  }
+  
+  if (doAutoscale) {
+    cis <- autoscale(cis)
+  }
+  
+  return(cis)
+
+}
+
 # Suppress checking notes for variables loaded at runtime from .RData files
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("s", "base_faces", "stimuli_params"))
 
